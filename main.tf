47ff9cacd6a71f99b2f22d72f135f8b7ca7da406
hesblac <olaiyagbolagade@gmail.com>
2023-04-03 17:28:30 +0100

    s

--- main.tf
+++ main.tf
@@ -2,19 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "b" {
-  bucket = "my-bucket13222"
+resource "aws_s3_bucket" "bukiy" {
+  bucket = "my-bucket4332"
 
   tags = {
     Environment = "Production"
   }
 }
 
-resource "aws_s3_bucket_acl" "b" {
-  bucket = aws_s3_bucket.b.id
-  grants {
-    id = "123456789012"
-    type = "CanonicalUser"
-    permission = "FULL_CONTROL"
-  }
-}
