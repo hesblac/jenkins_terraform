bec861540ceea8e9e390f416691c0d223137c827
hesblac <olaiyagbolagade@gmail.com>
2023-04-03 17:18:26 +0100

    s

--- main.tf
+++ main.tf
@@ -1,19 +1,20 @@
 provider "aws" {
-  profile = "myaws"
-  region  = var.region
+  region = "us-east-1"
 }
 
-
 resource "aws_s3_bucket" "b" {
-  bucket = "myawsbucket12345554555"
+  bucket = "my-bucket"
 
   tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
+    Environment = "Production"
   }
 }
 
-variable "region" {
-
-  
+resource "aws_s3_bucket_acl" "b" {
+  bucket = aws_s3_bucket.example.id
+  grants {
+    id = "123456789012"
+    type = "CanonicalUser"
+    permission = "FULL_CONTROL"
+  }
 }
