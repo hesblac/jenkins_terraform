36bc980ec841fecdec9a47ff62d26ebc415c0162
hesblac <olaiyagbolagade@gmail.com>
2023-04-07 15:09:20 +0100

    the codedit

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "monicrosielovie" {
-  bucket = "my-buoyylovniggaraw"
+  bucket = "my-buoyylovniggarawsomethingmustbuy"
 
   tags = {
     Environment = "Production"
